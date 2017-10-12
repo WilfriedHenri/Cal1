5faa7dba8476dddb894affd69f5cb5012015bdba
Student <Student@ZAPRE-D00000156.DirectDelivery.com>
2017-10-12 10:58:48 +0200

    additional work 1

--- src/cal1/Cal1.java
+++ src/cal1/Cal1.java
@@ -11,7 +11,10 @@ package cal1;
  */
 public class Cal1 {
     public int Add (String s1, String s2){
-        return 0;
+        int d1 = Integer.parseInt(s1);
+        int d2 = Integer.parseInt(s2);
+                
+        return d1+d2;
     }
     /**
      * @param args the command line arguments
